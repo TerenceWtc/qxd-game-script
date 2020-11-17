2b200fd8100f6842f961c2fe170a2740212d728d
赤白 <1464652235@qq.com>
2020-11-17 15:21:11 +0800

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,13 +1,14 @@
 /* eslint-disable import/no-dynamic-require */
 
 const env = process.env.NODE_ENV || 'dev';
+const port = process.env.NODE_PORT || 3000;
 const envConfig = require(`./config/${env}.json`);
 const constantConfig = require('./config/constant.json');
 const settingsConfig = require('./config/settings.json');
 const bankConfig = require('./config/bank.json');
 
 const config = {
-    port: 3001,
+    port,
     errorCode: {
         COMMON_400: 'E401 Bad Reqeust',
         COMMON_500: 'E501 Server Internal Error',
